46253f10858c9291e24344b967c3de98f72852bd
Bobby <bobur0218programmer@gmail.com>
2025-02-05 01:29:18 +0500

    init

---
+++ frontend/src/App.vue
@@ -0,0 +1,15 @@
+<script>
+import { RouterView } from 'vue-router'
+
+export default { name: "App" };
+</script>
+
+<template>
+  <nav>
+    <RouterLink to="/">Home</RouterLink>
+    <RouterLink to="/about">About</RouterLink>
+    <RouterLink to="/dynamic/25">Dynamic route</RouterLink>
+    <RouterLink to="/blabla">Some not found page</RouterLink>
+  </nav>
+  <RouterView />
+</template>
---
+++ frontend/src/pages/DynamicRoute.vue
@@ -0,0 +1,14 @@
+<script lang="ts">
+export default {
+  props: {
+    id: {
+      type: String,
+      required: true,
+    }
+  }
+}
+</script>
+
+<template>
+  <h1>dynamic route: {{ id }}</h1>
+</template>
